2d07285f6026ba4fccfca18cb9422a1ba1d48867
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-05-03 08:16:46 -0700

    Create SATYAUSERBOT.py

---
+++ SATYAUSERBOT.py
@@ -0,0 +1,38 @@
+from satyauserbot .utils.clints
+from userbot telethon.clinet
+from LEGEND-LX/USERBOT-KING/ /root/userbot.os
+import userbot satyabot
+    
+             
+                        
+                SatyaBot==int(SATYABOT(str(("Legend-Owner")).(event:).get
+                 SatyaBot_mirror_mod=False:
+                          mirror_mod=true:
+                  False
+   satyauserbot()
+                 try 
+                                       pass:(os.event.get).string_sessoin
+                                       send_messege_mod==False"
+                   mirror=1
+                   mirror=userbot.satya.user_ownerbot
+                          clint-telethon-telegram:await("string_userbot")
+                     os.event.get(var).get pip install python,
+                      (hack).sending>"""
+ None,
+ 
+legendownerbot======(int(legend-satya-owner-bot(("string_sessoin)))
+#userbot
+
+"""
+                                       ownerBot=legend(os.event.get(var)
+                                       mirror_hack+=(*op)
+                                       alive_msg ""
+                                       userbot_helper ""
+                                       bot_helper ""
+                                       var ""
+                                       botalive ""
+  print("strint_sessoin").var.bots
+  legend_satyu=pro_user(await:y
+                        await:x
+      sleep.time {5%}.fast_tut
+                        None,[error2]
